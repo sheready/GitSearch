86ebbb58ae67a6aa325905d3dfb363c1899ec9ef
sheready <csstlw@gmail.com>
2020-06-22 20:12:11 +0300

    add directive on events

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,6 +5,20 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+
 
 }
