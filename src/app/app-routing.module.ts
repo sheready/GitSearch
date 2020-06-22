e85041fd4e4aa1120ef5f001250416944791d910
sheready <csstlw@gmail.com>
2020-06-22 20:22:35 +0300

    add imports to route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [];
 
