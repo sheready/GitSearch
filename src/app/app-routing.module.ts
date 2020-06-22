4d70c08647489b87cc185ded14045730c8141518
sheready <csstlw@gmail.com>
2020-06-22 20:27:23 +0300

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {ProfileComponent } from './components/profile/profile.component';
 import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
